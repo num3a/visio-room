144d964d398bcd295faf2d5f4bf5bfd5488821bb
Emmanuel ERNEST <ernest.emmanuel@hotmail.fr>
2016-10-09 19:12:29 +0200

    add material-ui

--- imports/ui/containers/AppContainer.js
+++ imports/ui/containers/AppContainer.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 
-const AppContainer = (props) => (
-    <div>
-        <h1>THIS IS VISIO ROOM</h1>
-        <div>
-            {props.children}
-        </div>
-    </div>
-);
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+injectTapEventPlugin();
+
+class AppContainer extends Component {
+    render() {
+        return (
+            <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <div>
+                    <AppBar
+
+                        title="Hello"/>
+                    <Drawer
+                        docked={false}
+                        width={300}
+                        open={false}
+                    >
+                        <MenuItem>Home</MenuItem>
+                        <MenuItem>Profile</MenuItem>
+                    </Drawer>
+                    {this.props.children}
+                </div>
+            </MuiThemeProvider>
+        );
+    }
+}
 
 export default AppContainer;
--- server/main.js
+++ server/main.js
@@ -1,1 +1,14 @@
 import '/imports/startup/server';
+
+
+/*
+
+ Type de champ
+ CNAME
+ Sous domaine
+ www.visioroom.co
+ Cible
+ visioroom.co.
+
+
+ */
