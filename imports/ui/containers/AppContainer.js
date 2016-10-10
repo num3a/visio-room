f67256ae8bdadf9c7c6cd1be328d5647c6715575
Emmanuel ERNEST <ernest.emmanuel@hotmail.fr>
2016-10-10 13:17:33 +0200

    add some styles

--- imports/ui/containers/AppContainer.js
+++ imports/ui/containers/AppContainer.js
@@ -1,76 +1,82 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 
-import { Layout, Header, Navigation, Drawer, Content, Footer, FooterSection, FooterLinkList, FooterDropDownSection} from 'react-mdl';
+import { Layout, Button, Header, Navigation, Drawer, Content,
+    Footer, FooterSection, FooterLinkList, FooterDropDownSection,
+    Grid, Cell
+} from 'react-mdl';
 import { getColorClass, getTextColorClass} from '../utils/palette';
 class AppContainer extends Component {
 
     render() {
         return (
-           <div>
-               <Layout fixedHeader className={classNames(getColorClass('grey', 100), getTextColorClass('grey', 700))}>
-                   <Header className={getColorClass('primary')} title="Material Design Lite" scroll>
-                           <Navigation className={"mdl-layout--large-screen-only"}>
-                               <a href="#">Link</a>
-                               <a href="#">Link</a>
-                               <a href="#">Link</a>
-                               <a href="#">Link</a>
-                           </Navigation>
-                       </Header>
-                       <Drawer title="Title">
-                           <Navigation>
-                               <a href="#">Link</a>
-                               <a href="#">Link</a>
-                               <a href="#">Link</a>
-                               <a href="#">Link</a>
-                           </Navigation>
-                       </Drawer>
-                       <Content>
-                           <Footer size="mega">
-                               <FooterSection type="middle">
-                                   <FooterDropDownSection title="Features">
-                                       <FooterLinkList>
-                                           <a href="#">About</a>
-                                           <a href="#">Terms</a>
-                                           <a href="#">Partners</a>
-                                           <a href="#">Updates</a>
-                                       </FooterLinkList>
-                                   </FooterDropDownSection>
-                                   <FooterDropDownSection title="Details">
-                                       <FooterLinkList>
-                                           <a href="#">Specs</a>
-                                           <a href="#">Tools</a>
-                                           <a href="#">Resources</a>
-                                       </FooterLinkList>
-                                   </FooterDropDownSection>
-                                   <FooterDropDownSection title="Technology">
-                                       <FooterLinkList>
-                                           <a href="#">How it works</a>
-                                           <a href="#">Patterns</a>
-                                           <a href="#">Usage</a>
-                                           <a href="#">Products</a>
-                                           <a href="#">Contracts</a>
-                                       </FooterLinkList>
-                                   </FooterDropDownSection>
-                                   <FooterDropDownSection title="FAQ">
-                                       <FooterLinkList>
-                                           <a href="#">Questions</a>
-                                           <a href="#">Answers</a>
-                                           <a href="#">Contact Us</a>
-                                       </FooterLinkList>
-                                   </FooterDropDownSection>
-                               </FooterSection>
-                               <FooterSection type="bottom" logo="More Information">
-                                   <FooterLinkList>
-                                       <a href="https://developers.google.com/web/starter-kit/">Web Starter Kit</a>
-                                       <a href="#">Help</a>
-                                       <a href="#">Privacy & Terms</a>
-                                   </FooterLinkList>
-                               </FooterSection>
-                           </Footer>
-                       </Content>
-                   </Layout>
-           </div>
+            <div>
+                <Layout fixedHeader className={classNames(getColorClass('grey', 100), getTextColorClass('grey', 700))}>
+                    <Header className={getColorClass('primary')} title="Material Design Lite" scroll>
+                        <Navigation className={"mdl-layout--large-screen-only"}>
+                            <a href="#">Log In</a>
+                            <a href="#" >
+                                <Button raised ripple accent>Sign Up</Button>
+                            </a>
+                        </Navigation>
+                    </Header>
+                    <Drawer title="Title">
+                        <Navigation className={"mdl-layout--small-screen-only"}>
+                            <a href="#">Log In</a>
+                            <a href="#">Sign Up</a>
+                        </Navigation>
+                    </Drawer>
+                    <Content>
+                        <Grid className="demo-grid-1" >
+                            <Cell col={4}>4</Cell>
+                            <Cell col={4}>4</Cell>
+                            <Cell col={4}>4</Cell>
+                        </Grid>
+                        <Footer size="mega">
+                            <FooterSection type="middle">
+                                <FooterDropDownSection title="Features">
+                                    <FooterLinkList>
+                                        <a href="#">About</a>
+                                        <a href="#">Terms</a>
+                                        <a href="#">Partners</a>
+                                        <a href="#">Updates</a>
+                                    </FooterLinkList>
+                                </FooterDropDownSection>
+                                <FooterDropDownSection title="Details">
+                                    <FooterLinkList>
+                                        <a href="#">Specs</a>
+                                        <a href="#">Tools</a>
+                                        <a href="#">Resources</a>
+                                    </FooterLinkList>
+                                </FooterDropDownSection>
+                                <FooterDropDownSection title="Technology">
+                                    <FooterLinkList>
+                                        <a href="#">How it works</a>
+                                        <a href="#">Patterns</a>
+                                        <a href="#">Usage</a>
+                                        <a href="#">Products</a>
+                                        <a href="#">Contracts</a>
+                                    </FooterLinkList>
+                                </FooterDropDownSection>
+                                <FooterDropDownSection title="FAQ">
+                                    <FooterLinkList>
+                                        <a href="#">Questions</a>
+                                        <a href="#">Answers</a>
+                                        <a href="#">Contact Us</a>
+                                    </FooterLinkList>
+                                </FooterDropDownSection>
+                            </FooterSection>
+                            <FooterSection type="bottom" logo="More Information">
+                                <FooterLinkList>
+                                    <a href="https://developers.google.com/web/starter-kit/">Web Starter Kit</a>
+                                    <a href="#">Help</a>
+                                    <a href="#">Privacy & Terms</a>
+                                </FooterLinkList>
+                            </FooterSection>
+                        </Footer>
+                    </Content>
+                </Layout>
+            </div>
         );
     }
 }
