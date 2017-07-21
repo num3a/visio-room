73d9f224819229a97ffda41547d88e12e62550b4
Emmanuel ERNEST <ernest.emmanuel@hotmail.fr>
2017-07-21 18:25:22 +0200

    fix linting

--- imports/api/common/schema/location.js
+++ imports/api/common/schema/location.js
@@ -2,36 +2,36 @@ import SimpleSchema from 'simpl-schema';
 
 
 const LocationSchema = new SimpleSchema({
-    "type":{
-        type: String,
-        allowedValues: ["Point"]
-    },
-    "coordinates":{
-        type: Array,
-        minCount: 2,
-        maxCount: 2
-    },
-    "coordinates.$":{
-        type: Number,
-        custom: function(){
-            if(!(-90 <= this.value[0] <= 90))
-                return "lonOutOfRange" ;
-            if(!(-180 <= this.value[1] <= 180))
-                return "latOutOfRange" ;
-        }
+  "type":{
+    type: String,
+    allowedValues: ["Point"]
+  },
+  "coordinates":{
+    type: Array,
+    minCount: 2,
+    maxCount: 2
+  },
+  "coordinates.$":{
+    type: Number,
+    custom: function(){
+      if(!(-90 <= this.value[0] <= 90))
+        return "lonOutOfRange" ;
+      if(!(-180 <= this.value[1] <= 180))
+        return "latOutOfRange" ;
+    }
 
-    },
-    "name": {
-        type: String,
-        optional: true
-    },
+  },
+  "name": {
+    type: String,
+    optional: true
+  },
 });
 
 
 
 LocationSchema.messages = {
-    lonOutOfRange: 'Longitude out of range', // Must be between -90 and 90
-    latOutOfRange: 'Latitude out of range' // Must be between -180 and 180
+  lonOutOfRange: 'Longitude out of range', // Must be between -90 and 90
+  latOutOfRange: 'Latitude out of range' // Must be between -180 and 180
 };
 
 export default LocationSchema;
